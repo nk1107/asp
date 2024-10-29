1a6f4bd1713e33407498bd431d81430f30f7fcaa
Vidhi Singh <vidhisingh1956@gmail.com>
2024-10-29 14:47:42 +0530

    sign-in sign-out routes protection redirection

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div>
       
-      <Button>sign-in</Button>
-      <SignIn></SignIn>
+      <SignIn/>
+      
     </div>
   );
 }
--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -1,15 +1,16 @@
 
 import { signIn } from "@/auth"
+import { Button } from "./ui/button"
  
 export default function SignIn() {
   return (
     <form
       action={async () => {
         "use server"
-        await signIn("google")
+        await signIn("google",{redirectTo:"/dashboard"})
       }}
     >
-      <button type="submit">Signin with Google</button>
+      <Button type="submit">Signin with Google</Button>
     </form>
   )
 } 
