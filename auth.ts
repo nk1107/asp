1f5dc30414a4e2585eb808ad26ac41cac6409678
nk1107 <nishantkumar.nk11072004@gmail.com>
2024-10-29 10:28:55 +0000

    sign in to database

--- auth.ts
+++ auth.ts
@@ -1,9 +1,29 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { prisma } from "./db/db";
 
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  
+  callbacks: {
+    async signIn({ user }) {
+      // Check if the user already exists
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email as string },
+      });
+      
+      // If the user doesn't exist, create a new one
+      if (!existingUser) {
+        await prisma.user.create({
+          data: {
+            name: user.name as string,
+            email: user.email as string,
+          },
+        });
+      }
+
+      return true; // Continue sign-in process
+    },
+  },
 })
